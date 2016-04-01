8d8163e152d915af3f59319fb9eb17a811b3ef72
akumar18 <akumar18@ncsu.edu>
2016-04-01 13:25:36 -0400

    Seek Align Done

--- BountyHunter/src/movement/SeekAlign.java
+++ BountyHunter/src/movement/SeekAlign.java
@@ -4,14 +4,24 @@ import ds.KinematicDS;
 import processing.core.PVector;
 
 public class SeekAlign {
-	public KinematicDS update(KinematicDS character, int[] target){
+	public KinematicDS update(KinematicDS character, int[] target, int maxVelocity, float maxRotation){
 		KinematicDS output = character;
 		
 		//Seek
-		PVector direction = new PVector(character.position.x - target[0], character.position.y - target[1]);
-		
+		PVector velocity = new PVector(character.position.x - target[0], character.position.y - target[1]);
+		if(velocity.mag() > maxVelocity){
+			velocity.normalize();
+			velocity.mult(maxVelocity);
+		}
+		output.velocity = velocity;
 		
 		//Align
+		float targetOrientation = (float)Math.atan2(character.position.y - target[1], character.position.x - target[0]);
+		float rotation = targetOrientation - character.orientation;
+		if(rotation > maxRotation){
+			rotation = maxRotation*(rotation/Math.abs(rotation));
+		}
+		output.rotation = rotation;
 		
 		
 		return output;
